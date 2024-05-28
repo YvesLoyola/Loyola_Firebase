3f76469b19e600793b80bca90c00a09bcfac1acf
Yves Lloyd P. Loyola <144214030+YvesLoyola@users.noreply.github.com>
2024-05-28 22:07:23 +0800

    Add files via upload

---
+++ fireConfig.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyBDBI5IcGn0ZEZ_daVLS-c0AENo22YyBX0",
+  authDomain: "yves-crud.firebaseapp.com",
+  databaseURL: "https://yves-crud-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "yves-crud",
+  storageBucket: "yves-crud.appspot.com",
+  messagingSenderId: "509994501416",
+  appId: "1:509994501416:web:60812bcc04784c9cfc7c1d"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
